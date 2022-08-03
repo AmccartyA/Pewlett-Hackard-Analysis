fc4a32190bbe8c1796eb1b4c80ec5f68e8a52882
Aaron McCarty <amccartya@gmail.com>
2022-08-03 14:58:52 -0700

    Built new mentor Tables

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,50 +1,51 @@
--- Use Dictinct with Orderby to remove duplicate rows
---PART 1
+-- Part 1)Create a reitrment_titles table
 SELECT e.emp_no,
 e.first_name,
 e.last_name,
 t.title,
 t.from_date,
 t.to_date
-INTO retirement_table
-FROM employees as e
+INTO retirement_titles 
+FROM employees AS e
 INNER JOIN titles AS t
 ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no ASC;
 
 -- Use Dictinct with Orderby to remove duplicate rows 
+Drop Table unique_titles;
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
 rt.last_name,
 rt.title
-
 INTO unique_titles
 FROM retirement_titles AS rt
+WHERE (rt.to_date = '9999-01-01')
 ORDER BY emp_no ASC, to_date DESC;
 
---part 3SELECT 
-COUNT(ut.emp_no),ut.title
---INTO retiring_titles
-FROM unique_titles as ut
+--Select just titles and put into a new table
+SELECT COUNT(ut.emp_no),ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
 
---Deliverable part 2
-SELECT DISTINCT ON(e.emp_no)e.emp_no,
+SELECT * FROM retiring_titles;
+
+--Part 2)Make a mentorship List
+SELECT DISTINCT ON (e.emp_no)e.emp_no,
 e.first_name,
 e.last_name,
 e.birth_date,
 de.from_date,
 de.to_date,
-t.title
---DISTINCT ON(emp_no)
---INTO mentor_eligibility
+titles.title
+INTO mentorship_eligibility
 FROM employees AS e
 INNER JOIN dept_emp AS de
 ON (e.emp_no = de.emp_no)
-INNER JOIN titles AS t
-ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+INNER JOIN titles 
+ON (e.emp_no = titles.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
-ORDER BY emp_no;
+ORDER BY emp_no ASC;
